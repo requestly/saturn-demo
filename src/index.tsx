6f6b064bbc2f8f641b27e61c3ef7fadc53a66238
rohanmathur91 <mathurrohan04@gmail.com>
2024-02-09 12:00:44 +0530

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 
 const root = createRoot(document.getElementById("app"));
 
 root.render(
   <StrictMode>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </StrictMode>
 );
